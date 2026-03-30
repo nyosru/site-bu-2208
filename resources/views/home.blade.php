3b12de2d5bb5b754f5cc91b78f7c05d8ca2553cf
nyosru <nyos@rambler.ru>
2026-03-30 06:56:18 +0500

    ++ test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,5 +4,6 @@
     <section class="max-w-[760px] rounded-[14px] border border-gray-300 bg-white p-6">
         <h1>Доска объявлений</h1>
         <p>Публикуйте и находите нужные объявления быстро.</p>
+        dev
     </section>
 @endsection
